f6e45cd1cf06e3983a519b65dc2fc8762ec1e513
khaled0915 <khaledsaifullah50956@gmail.com>
2023-10-06 11:47:40 +0600

    creted login and register route and make tha login and regitser page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">react-auth</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
---
+++ src/__COntext__api.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * 1. create context  and export 
+ * 
+ * 2 . create provider 
+ * 
+ * 3 . set value 
+ * 
+ * 4 . useCOntext
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
